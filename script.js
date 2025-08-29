3ec72797824a326f80636ba1266aa68e2d51f2a2
diko8824-bit <diko8824@gmail.com>
2025-08-29 11:33:09 +0330

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll
-const links = document.querySelectorAll('.navbar a');
-links.forEach(link => {
-  link.addEventListener('click', e=>{
-    e.preventDefault();
-    document.querySelector(link.getAttribute('href')).scrollIntoView({behavior:'smooth'});
-  });
-});
